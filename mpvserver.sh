98a895a6a8004dcb6faa020d6fc44d4df528c145
Christophe <88241389+cdebrosse@users.noreply.github.com>
2023-02-26 09:58:50 +0100

    Update mpvserver.sh
    documentation

--- mpvserver.sh
+++ mpvserver.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
-
+# boucle infinie lancé en mode démon, le process tourne en tache de fonds et vérifie régulièrement (sleep 200 : temps de pause)
+# l'état de pulseaudio : programme principal qui diffuse le son
+# et l'état de mpv : transmet la musique au travers du réseau
 while true
 do
 MPV=$(ps -ef | grep " mpv "| grep -v grep | wc -l )
 PUL=$(ps -ef | grep pulseaudio | grep -v grep | wc -l)
 echo "mpv : $MPV , pul : $PUL
 "
+# si mpv ne tourne pas 
 if [ $MPV -eq 0 ]
 then
-
+	# on vérifie si pulse tourne
 	if [ $PUL -eq 0 ]
 	then
+		# pulseaudio ne tourne pas il faut le lancer
 		pulseaudio -D 
 		RC=$?
 		if [ $RC -eq 0 ]
 		then
 			echo "pulse démarre"
 		else
+			# tentative avortée de démarrage
 			echo "erreur pulseaudio"
 			exit 0
 		fi
 	else
+		# tout va bien pour pulse
 		echo "pulse déjà démarré"
 	fi
+	# réactualisation d'une playlist aléatoire à planifier via crontab pour réactualisation planifiée
 	find /srv/musique/musique/ -type f ! -wholename "*podcasts*" | grep -P 'mp3$|flac$' | shuf -n 30 > /tmp/listjour.m3u
+	#   /emplacement réseau nas/                                       /fin nom fichiers/                /nom playliste/
+	#                                 /enlève les podcasts de la liste/                       /nombre de titres 30/
 	nohup mpv --script-opts=webui-port=8078 --pause /tmp/listjour.m3u --vo=null --script=~/.config/mpv/scripts/webui/main.lua > /tmp/log_mpv.log &
+	#                             /port web/           /playliste/                                                              /log de mpv/
 	echo "mpv démarre"
 	
 else
